d2373ee6be6eb1bd6402c0b8290ee71d40bb8d4b
Abdullah Çok <me@abdullahcok.com>
2020-06-08 08:10:38 +0300

    header added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import './App.css';
-import {Route} from 'react-router-dom'
+import {Switch, Route} from 'react-router-dom'
 
 import HomePage from './Pages/HomePage/HomePage.jsx';
 import ShopPage from './Pages/ShopPage/ShopPage.jsx';
+import Header from './Components/Header/Header.jsx';
 
 
 
 function App() {
   return (
       <div>
-        <Route exact path='/' component={HomePage}/>
-        <Route path='/shop' component={ShopPage}/>
+        <Header/>
+        <Switch>
+          <Route exact path='/' component={HomePage}/>
+          <Route path='/shop' component={ShopPage}/>
+        </Switch>
       </div>
   );
 }
